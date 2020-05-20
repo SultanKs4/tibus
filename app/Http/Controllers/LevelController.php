8d323531fa4f0c540b4dda84ad61f8b0d836ff0c
SultanKs4 <sultangendut@gmail.com>
2020-05-20 12:23:16 +0700

    feat(api): add crud endpoint level

---
+++ app/Http/Controllers/LevelController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Level;
+
+class LevelController extends Controller
+{
+    public function get($id = null)
+    {
+        if ($id == null) {
+            $data = Level::all();
+        } else {
+            $data = Level::where('id', $id)->get();
+        }
+
+        if (count($data) > 0) {
+            $res['status'] = true;
+            $res['data'] = $data;
+            return response($res);
+        } else {
+            $res['status'] = false;
+            return response($res);
+        }
+    }
+
+    public function create(Request $request)
+    {
+        $akun = new Level();
+        $akun->name = $request->name;
+
+        if ($akun->save()) {
+            $res['status'] = true;
+            $res['message'] = "Data level ditambahkan";
+            return response($res);
+        } else {
+            $res['status'] = false;
+            $res['message'] = "gagal membuat data level";
+            return response($res);
+        }
+    }
+
+    public function update(Request $request)
+    {
+        $data = Level::find($request->id);
+        $data->name = $request->name;
+
+        if ($data->save()) {
+            $res['status'] = true;
+            $res['message'] = "Data level diubah";
+            return response($res);
+        } else {
+            $res['status'] = false;
+            $res['message'] = "gagal mengubah data level";
+            return response($res);
+        }
+    }
+
+    public function delete(Request $request)
+    {
+        if ($request->id == null) {
+            $res['status'] = false;
+            $res['message'] = "id not provided!";
+            return response($res);
+        } else {
+            $data = Level::where('id', $request->id);
+
+            if ($data->delete()) {
+                $res['status'] = true;
+                $res['message'] = "akun deleted";
+                return response($res);
+            } else {
+                $res['status'] = false;
+                $res['message'] = "akun fail to delete";
+                return response($res);
+            }
+        }
+    }
+}
