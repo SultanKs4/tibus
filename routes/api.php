8de88ac2fc5f8aad92a0048b3cc090abbb8e0a8e
SultanKs4 <sultangendut@gmail.com>
2020-05-22 05:42:59 +0700

    feat(middleware): add middleware apikey

--- routes/api.php
+++ routes/api.php
@@ -18,82 +18,84 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('akun', 'AkunController@get');
+Route::middleware('apikey')->group(function () {
+    Route::get('akun', 'AkunController@get');
 
-Route::post('akun/check', 'AkunController@login');
+    Route::post('akun/check', 'AkunController@login');
 
-Route::post('akun', 'AkunController@create');
+    Route::post('akun', 'AkunController@create');
 
-Route::put('akun', 'AkunController@update');
+    Route::put('akun', 'AkunController@update');
 
-Route::delete('akun', 'AkunController@delete');
+    Route::delete('akun', 'AkunController@delete');
 
-Route::get('level', 'LevelController@get');
+    Route::get('level', 'LevelController@get');
 
-Route::post('level', 'LevelController@create');
+    Route::post('level', 'LevelController@create');
 
-Route::put('level', 'LevelController@update');
+    Route::put('level', 'LevelController@update');
 
-Route::delete('level', 'LevelController@delete');
+    Route::delete('level', 'LevelController@delete');
 
-Route::get('method', 'MethodController@get');
+    Route::get('method', 'MethodController@get');
 
-Route::post('method', 'MethodController@create');
+    Route::post('method', 'MethodController@create');
 
-Route::put('method', 'MethodController@update');
+    Route::put('method', 'MethodController@update');
 
-Route::delete('method', 'MethodController@delete');
+    Route::delete('method', 'MethodController@delete');
 
-Route::get('payment', 'PaymentController@get');
+    Route::get('payment', 'PaymentController@get');
 
-Route::post('payment', 'PaymentController@create');
+    Route::post('payment', 'PaymentController@create');
 
-Route::put('payment', 'PaymentController@update');
+    Route::put('payment', 'PaymentController@update');
 
-Route::delete('payment', 'PaymentController@delete');
+    Route::delete('payment', 'PaymentController@delete');
 
-Route::get('po', 'PoController@get');
+    Route::get('po', 'PoController@get');
 
-Route::post('po', 'PoController@create');
+    Route::post('po', 'PoController@create');
 
-Route::put('po', 'PoController@update');
+    Route::put('po', 'PoController@update');
 
-Route::delete('po', 'PoController@delete');
+    Route::delete('po', 'PoController@delete');
 
-Route::get('status', 'StatusController@get');
+    Route::get('status', 'StatusController@get');
 
-Route::post('status', 'StatusController@create');
+    Route::post('status', 'StatusController@create');
 
-Route::put('status', 'StatusController@update');
+    Route::put('status', 'StatusController@update');
 
-Route::delete('status', 'StatusController@delete');
+    Route::delete('status', 'StatusController@delete');
 
-Route::get('terminal', 'TerminalController@get');
+    Route::get('terminal', 'TerminalController@get');
 
-Route::post('terminal', 'TerminalController@create');
+    Route::post('terminal', 'TerminalController@create');
 
-Route::put('terminal', 'TerminalController@update');
+    Route::put('terminal', 'TerminalController@update');
 
-Route::delete('terminal', 'TerminalController@delete');
+    Route::delete('terminal', 'TerminalController@delete');
 
-Route::get('trayek', 'TrayekController@get');
+    Route::get('trayek', 'TrayekController@get');
 
-Route::get('trayek/search', 'TrayekController@get');
+    Route::get('trayek/search', 'TrayekController@get');
 
-Route::post('trayek', 'TrayekController@create');
+    Route::post('trayek', 'TrayekController@create');
 
-Route::put('trayek', 'TrayekController@update');
+    Route::put('trayek', 'TrayekController@update');
 
-Route::delete('trayek', 'TrayekController@delete');
+    Route::delete('trayek', 'TrayekController@delete');
 
-Route::get('tiket', 'TiketController@get');
+    Route::get('tiket', 'TiketController@get');
 
-Route::get('tiket/booked', 'TiketController@getBooked');
+    Route::get('tiket/booked', 'TiketController@getBooked');
 
-Route::post('tiket', 'TiketController@create');
+    Route::post('tiket', 'TiketController@create');
 
-Route::post('tiket/trans', 'TiketController@trans');
+    Route::post('tiket/trans', 'TiketController@trans');
 
-Route::put('tiket', 'TiketController@update');
+    Route::put('tiket', 'TiketController@update');
 
-Route::delete('tiket', 'TiketController@delete');
+    Route::delete('tiket', 'TiketController@delete');
+});
